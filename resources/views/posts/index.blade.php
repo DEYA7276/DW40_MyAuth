695c6379d7bf0eebaa17d986b7717ec4a492f9be
deyanira <23610030@utgz.edu.mx>
2025-04-07 22:08:31 -0600

    Commit 2

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -108,15 +108,16 @@
                                             </p>
                                             {{-- botones de confirmacion y cancelacion --}}
                                             <div class="mt-4 flex justify-end space-x-2">
-                                                <button x-on:click="showConfirm + false" class="px-4 py-2 bg-gray-300 dark:text-gray-200 rounded hover:bg-gray-400 
+                                                <button x-on:click="showConfirm = false" class="px-4 py-2 bg-gray-300 dark:text-gray-200 rounded hover:bg-gray-400 
                                                 dark:hover:bg-gray-60">
                                                     {{__('No, Keep it!')}}
                                                 </button>
 
-                                                <button x-on:click="document.getElementById('delete-post-form-{{$post->id}}').submit()"
-                                                    class="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-70">
+                                                <button x-on:click="document.getElementById('delete-post-form{{$post->id}}').submit()"
+                                                    class="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700">
                                                     {{__('Yes, Delete it!') }}
                                                 </button>
+                                                
                                             </div>
                                         </div>
                                     </div>
